9d58afa33a9caf3e5a142e4dc2d5f2a8d11d5616
dali216 <dhaouadidoudiclub@gmail.com>
2026-09-17 22:33:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -550,5 +550,43 @@ supabaseClient.auth
         event
       );
 
+      /* =========================
+   PUSH NOTIFICATIONS
+========================= */
+
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener(
+    "load",
+    async () => {
+
+      try {
+
+        const registration =
+          await navigator.serviceWorker.register(
+            "sw.js",
+            {
+              scope: "./"
+            }
+          );
+
+        console.log(
+          "✅ Service Worker actif:",
+          registration.scope
+        );
+
+      } catch (error) {
+
+        console.error(
+          "❌ Service Worker error:",
+          error
+        );
+
+      }
+
+    }
+  );
+
+}
     }
   );
