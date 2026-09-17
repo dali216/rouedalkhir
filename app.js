854a64cf3760c7480f4bba2fdce8820d89041f52
dali216 <dhaouadidoudiclub@gmail.com>
2026-09-17 22:37:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -590,3 +590,42 @@ if ("serviceWorker" in navigator) {
 }
     }
   );
+
+/* =========================
+   PUSH NOTIFICATIONS
+========================= */
+
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener(
+    "load",
+    async () => {
+
+      try {
+
+        const registration =
+          await navigator.serviceWorker.register(
+            "sw.js",
+            {
+              scope: "./"
+            }
+          );
+
+        console.log(
+          "✅ Service Worker actif:",
+          registration.scope
+        );
+
+      } catch (error) {
+
+        console.error(
+          "❌ Service Worker error:",
+          error
+        );
+
+      }
+
+    }
+  );
+
+}
